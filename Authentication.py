9a1c7716557e92c98a8dff963374e29d62eb58e6
Kareem Iskander (kiskande) <kiskande@cisco.com>
2022-06-08 12:55:37 -0700

    Code Commit

---
+++ Authentication.py
@@ -0,0 +1,31 @@
+"""
+Copyright (c) 2020 Cisco and/or its affiliates.
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). You may obtain a copy of the
+License at
+               https://developer.cisco.com/docs/licenses
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+"""
+
+__author__ = "Kareem Iskander, Lead Tech Advocate"
+__version__ = "0.1.0"
+__copyright__ = "Copyright (c) 2022 Cisco and/or its affiliates."
+__license__ = "Cisco Sample Code License, Version 1.1"
+
+
+from dnacentersdk import DNACenterAPI
+
+dnac_creds = {}
+dnac_creds['url'] = 'https://sandboxdnac2.cisco.com'
+dnac_creds['username'] = 'devnetuser'
+dnac_creds['password'] = 'Cisco123!'
+
+
+if __name__ == '__main__':
+    dnac = DNACenterAPI(username=dnac_creds['username'], password=dnac_creds['password'], base_url=dnac_creds['url'], verify=False)
+    print("Auth Token: ", dnac.access_token)
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# ciscolive-devwks-2107
+# Introduction
+
+Welcome to **Cisco DNA Center SDK in action** workshop. 
+This is an *introductory* workshop so if you have played with Cisco DNA Center APIs you are in the right place! and if you have not, that's ok too!
+
+
+### Session Rules
+- No boring slides, all of the required tooling will be within your in-browser developer environment
+- Interruptions, questions and heckling are welcomed and encouraged 
+- Have fun!
+
+
+# Learning Objectives
+
+- Define SDK
+- API vs SDK
+- Setup Cisco DNA Center SDK
+- Define and Build a poor man's config. manager
+
